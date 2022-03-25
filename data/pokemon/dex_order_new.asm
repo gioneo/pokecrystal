396c32d65d9bba516137c451c2a3ae5f6c077ba8
gioneo <102384790+gioneo@users.noreply.github.com>
2022-03-25 15:03:51 -0400

    Update dex_order_new.asm

--- data/pokemon/dex_order_new.asm
+++ data/pokemon/dex_order_new.asm
@@ -11,15 +11,8 @@ NewPokedexOrder:
 	db TOTODILE
 	db CROCONAW
 	db FERALIGATR
-	db PIDGEY
-	db PIDGEOTTO
-	db PIDGEOT
-	db SPEAROW
-	db FEAROW
 	db HOOTHOOT
 	db NOCTOWL
-	db RATTATA
-	db RATICATE
 	db SENTRET
 	db FURRET
 	db PICHU
@@ -35,9 +28,6 @@ NewPokedexOrder:
 	db LEDIAN
 	db SPINARAK
 	db ARIADOS
-	db GEODUDE
-	db GRAVELER
-	db GOLEM
 	db ZUBAT
 	db GOLBAT
 	db CROBAT
@@ -49,10 +39,6 @@ NewPokedexOrder:
 	db WIGGLYTUFF
 	db TOGEPI
 	db TOGETIC
-	db SANDSHREW
-	db SANDSLASH
-	db EKANS
-	db ARBOK
 	db DUNSPARCE
 	db MAREEP
 	db FLAAFFY
@@ -88,20 +74,12 @@ NewPokedexOrder:
 	db GLOOM
 	db VILEPLUME
 	db BELLOSSOM
-	db DROWZEE
-	db HYPNO
 	db ABRA
 	db KADABRA
 	db ALAKAZAM
 	db DITTO
 	db PINECO
 	db FORRETRESS
-	db NIDORAN_F
-	db NIDORINA
-	db NIDOQUEEN
-	db NIDORAN_M
-	db NIDORINO
-	db NIDOKING
 	db YANMA
 	db SUNKERN
 	db SUNFLORA
@@ -109,8 +87,6 @@ NewPokedexOrder:
 	db EXEGGUTOR
 	db SUDOWOODO
 	db WOBBUFFET
-	db VENONAT
-	db VENOMOTH
 	db SCYTHER
 	db SCIZOR
 	db PINSIR
@@ -133,10 +109,6 @@ NewPokedexOrder:
 	db STANTLER
 	db MARILL
 	db AZUMARILL
-	db DIGLETT
-	db DUGTRIO
-	db MANKEY
-	db PRIMEAPE
 	db MEOWTH
 	db PERSIAN
 	db PSYDUCK
@@ -180,7 +152,6 @@ NewPokedexOrder:
 	db SEEL
 	db DEWGONG
 	db LICKITUNG
-	db TANGELA
 	db EEVEE
 	db VAPOREON
 	db JOLTEON
@@ -200,15 +171,8 @@ NewPokedexOrder:
 	db DONPHAN
 	db MANTINE
 	db SKARMORY
-	db DODUO
-	db DODRIO
 	db PONYTA
 	db RAPIDASH
-	db CUBONE
-	db MAROWAK
-	db KANGASKHAN
-	db RHYHORN
-	db RHYDON
 	db MURKROW
 	db HOUNDOUR
 	db HOUNDOOM
@@ -220,37 +184,14 @@ NewPokedexOrder:
 	db PORYGON2
 	db CHANSEY
 	db BLISSEY
-	db LAPRAS
-	db OMANYTE
-	db OMASTAR
-	db KABUTO
-	db KABUTOPS
-	db AERODACTYL
 	db SNORLAX
-	db BULBASAUR
-	db IVYSAUR
-	db VENUSAUR
-	db CHARMANDER
-	db CHARMELEON
-	db CHARIZARD
-	db SQUIRTLE
-	db WARTORTLE
-	db BLASTOISE
-	db ARTICUNO
-	db ZAPDOS
-	db MOLTRES
 	db RAIKOU
 	db ENTEI
 	db SUICUNE
-	db DRATINI
-	db DRAGONAIR
-	db DRAGONITE
 	db LARVITAR
 	db PUPITAR
 	db TYRANITAR
 	db LUGIA
 	db HO_OH
-	db MEWTWO
-	db MEW
 	db CELEBI
 	assert_table_length NUM_POKEMON
